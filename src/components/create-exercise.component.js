b795bc3c4472b99a27baf08bccf9987c239cb29d
Calvin Wijaya <47976609+crshao@users.noreply.github.com>
2021-01-04 12:57:19 +0700

    Connecting front-end to back-end, inserting to mongoDB

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -24,10 +25,16 @@ export default class CreateExercise extends Component {
     }
 
     componentDidMount() {
-        this.setState({
-            users: ['test user'],
-            username: 'test user'
-        });
+        axios.get('http://localhost:5000/users')
+            .then(response => {
+                if(response.data.length > 0)
+                {
+                    this.setState({
+                        users: response.data.map(user => user.username),
+                        username: response.data[0].username
+                    });
+                }
+            });
     }
 
     onChangeUsername(e) {
@@ -64,6 +71,15 @@ export default class CreateExercise extends Component {
             date: this.state.date
         }
         console.log(exercise);
+        axios.post('http://localhost:5000/exercises/add', exercise)
+            .then(res => console.log(res.data));
+
+        // this.setState({
+        //     username: '',
+        //     description: '',
+        //     duration: 0,
+        //     date: new Date()
+        // })
 
         window.location = '/';
     }
